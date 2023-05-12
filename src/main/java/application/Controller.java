acd97be739a0484eb7793ef647ed050e0165a10e
ArianitSGashi <arianit.gashi9@student.uni-pr.edu>
2023-05-12 22:05:51 +0200

    commit2

--- src/main/java/application/Controller.java
+++ src/main/java/application/Controller.java
@@ -1,5 +1,11 @@
+
+package application;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller {
     Implementation cipher = new Implementation();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,12 @@
 module com.example.des_al_imp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     requires org.kordamp.bootstrapfx.core;
 
     opens com.example.des_al_imp to javafx.fxml;
+    opens application to javafx.fxml;
     exports com.example.des_al_imp;
+    exports application to javafx.graphics;
 }
